b457fda3b07db0287d16be24b9c972b542e5df77
Nathan McCutchen <n8wm@icloud.com>
2024-03-15 22:11:07 -0700

    done with schemas (hopefully)

--- database/schemas/__init__.py
+++ database/schemas/__init__.py
@@ -4,17 +4,22 @@ import json
 from typing import Any
 
 
-def load_json(file_path: str) -> dict[str, Any]:
-    with open(file_path, "r") as file:
-        return json.load(file)
+__cache: dict[str, dict[str, Any]] = {}
 
 
-term = load_json("database/schemas/terms.json")
-building = load_json("database/schemas/buildings.json")
-room = load_json("database/schemas/rooms.json")
-college = load_json("database/schemas/colleges.json")
-subject = load_json("database/schemas/subjects.json")
-course = load_json("database/schemas/courses.json")
-section = load_json("database/schemas/sections.json")
-ge = load_json("database/schemas/ges.json")
-instructor = load_json("database/schemas/instructors.json")
+def __load_json(file_path: str) -> dict[str, Any]:
+    if file_path not in __cache:
+        with open(file_path, "r") as file:
+            __cache[file_path] = {"$jsonSchema": json.load(file)}
+    return __cache[file_path]
+
+
+term = __load_json("database/schemas/terms.json")
+building = __load_json("database/schemas/buildings.json")
+room = __load_json("database/schemas/rooms.json")
+college = __load_json("database/schemas/colleges.json")
+subject = __load_json("database/schemas/subjects.json")
+course = __load_json("database/schemas/courses.json")
+section = __load_json("database/schemas/sections.json")
+ge = __load_json("database/schemas/ges.json")
+instructor = __load_json("database/schemas/instructors.json")
